2661f7fb3f626fadbb677c382d198f9b4622caae
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-09-08 22:59:06 +0700

    Add trailer section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import { FeatureSection } from "../components/FeatureSection";
 import { HeroSection } from "../components/HeroSection";
+import { TrailersSection } from "../components/TrailersSection";
 
 export function Home() {
   return (
     <>
       <HeroSection />
       <FeatureSection />
+      <TrailersSection />
     </>
   );
 }
